74e08039f640c224957dc7b6ba2ac34e839c765d
Stephanie Ralls <stephanieralls@Stephanies-MacBook-Pro-2.local>
2016-11-06 11:36:26 -0700

    Changes to navbar including login and sign up. Not hooked up.

--- src/navBar.jsx
+++ src/navBar.jsx
@@ -16,44 +16,48 @@ render() {
               <span className="icon-bar"></span>
               <span className="icon-bar"></span>
             </button>
-            <a className="navbar-brand" href="#">Brand</a>
+            <a className="navbar-brand" href="#">Gamehound</a>
           </div>
 
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav">
-              <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-              <li><a href="#">Link</a></li>
-              <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                <ul className="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" className="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                  <li role="separator" className="divider"></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
+              <li className="active"><a href="#">About<span className="sr-only">(current)</span></a></li>
+              <li><a href="#">Contact Us</a></li>
             </ul>
 
-            <form className="navbar-form navbar-left">
-              <div className="form-group">
-                <input type="text" className="form-control" placeholder="Search"/>
-              </div>
-              <button type="submit" className="btn btn-default">Submit</button>
-            </form>
             <ul className="nav navbar-nav navbar-right">
-              <li><a href="#">Link</a></li>
+              <li form className="navbar-form navbar-right" role="search">
+                <div className="form-group">
+                    <input type="text" className="form-control" name="username" placeholder="Username"/>
+                </div>
+                <div className="form-group">
+                    <input type="text" className="form-control" name="password" placeholder="Password"/>
+                </div>
+                <button type="submit" className="btn btn-default">Sign In</button>
+              </li> 
               <li className="dropdown">
-                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-                <ul className="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" className="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                </ul>
+                <a href="#" className="dropdown-toggle" data-toggle="dropdown"><b>Dont have an account?</b> <span className="caret"></span></a>
+                  <ul id="login-dp" className="dropdown-menu">
+                    <li>
+                     <div className="row">
+                        <div className="col-md-12">
+                           <form className="form" role="form" method="post" action="signup" accept-charset="UTF-8" id="signup-nav">
+                              <div className="form-group">
+                                 <label className="sr-only" for="exampleInputEmail2">Email address</label>
+                                 <input type="email" className="form-control" id="exampleInputEmail2" placeholder="Email address" required/>
+                              </div>
+                              <div className="form-group">
+                                 <label className="sr-only" for="exampleInputPassword2">Password</label>
+                                 <input type="password" className="form-control" id="exampleInputPassword2" placeholder="Password" required/>            
+                              </div>
+                              <div className="form-group" id="signup-btn">
+                                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
+                              </div>
+                           </form>
+                        </div>
+                     </div>
+                    </li>
+                  </ul>
               </li>
             </ul>
           </div>
