5ef32e1230b9eaf2eda08761f26cf3fa72935862
Stephanie Ralls <stephanieralls@Stephanies-MacBook-Pro-2.local>
2016-11-08 16:01:34 -0700

    Heroku initialization

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'mongodb://localhost:27017/gamehound' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : process.env.MONGODB_URI || 'mongodb://localhost:27017/gamehound' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
