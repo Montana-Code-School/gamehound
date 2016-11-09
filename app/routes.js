426bb726c4a2ed3d76f96fcbed7337e29808297b
Stephanie Ralls <stephanieralls@Stephanies-MacBook-Pro-2.local>
2016-11-09 16:17:05 -0700

    Updated changes made to fun advisor that didn't get pushed to gamehound. Back to normal

--- app/routes.js
+++ app/routes.js
@@ -2,26 +2,26 @@ var Game = require('./models/Game');
 
 module.exports = function(app, passport) {
 
-	app.post('/api/game', function (req, res) {
-		new Game({
-		gameName: req.body.gameName,
-		numPlayers: req.body.numPlayers.list, //needs to be array
-		type: req.body.type.list, //needs to be array
-		time: req.body.time,
-		description: req.body.description,
-		itemsNeeded: req.body.itemsNeeded.list, //needs to be array
-		tutorial: req.body.tutorial.list, //needs to be array
-		rating: req.body.rating, 
-		difficulty: req.body.difficulty
-		})
-		.save(function(err, data) {
-			if (err) {
-			console.error("Savage burn: ", err)
-			} else {
-			res.json(data)
-			}
-		})
-	})
+    app.post('/api/game', function (req, res) {
+        new Game({
+        gameName: req.body.gameName,
+        numPlayers: req.body.numPlayers.list, //needs to be array
+        type: req.body.type.list, //needs to be array
+        time: req.body.time,
+        description: req.body.description,
+        itemsNeeded: req.body.itemsNeeded.list, //needs to be array
+        tutorial: req.body.tutorial.list, //needs to be array
+        rating: req.body.rating, 
+        difficulty: req.body.difficulty
+        })
+        .save(function(err, data) {
+            if (err) {
+            console.error("Savage burn: ", err)
+            } else {
+            res.json(data)
+            }
+        })
+    })
 
     app.post('/api/game/recommend', function(req, res){
         Game.createFilterScore({difficulty:req.body.difficulty,
@@ -31,7 +31,6 @@ module.exports = function(app, passport) {
             ,function(err, sortedGames){
                 res.json(sortedGames)
         })
-
     })
     
     app.post('/signup', passport.authenticate('local-signup', {
@@ -58,16 +57,4 @@ module.exports = function(app, passport) {
             res.json({loggedIn: false, flash: req.flash()})
         }
     })
-
 }
-
-
-// gameName:"Count to 20",
-// numPlayers:8,
-// type:["Road Trip","Improv"],
-// time:10,
-// description:"Count to 20 without repeating self",
-// itemsNeeded:["none"],
-// tutorial:["1. ablas", "2. asdofijasdfijo"],
-// rating:5,
-// difficulty:"Hard"
--- src/renderedGames.jsx
+++ src/renderedGames.jsx
@@ -9,43 +9,43 @@ var request = requests.request
 
 class RenderedGames extends React.Component {
 
-	constructor(props) {
+  constructor(props) {
   super(props);
 
   this.state = {type: new ToggleList(),
-  			       	renderedGames: null
-  				     }
+                renderedGames: null
+               }
   }
 
   stateToggler(newType){
-  	this.setState({type: this.state.type.toggle(newType)})
+    this.setState({type: this.state.type.toggle(newType)})
   }
 
-	dontShowEmpty(prop){ //Can maybe use to generalize if needed? 
-		if(prop.length !== 0){
-			return (<div className="col-md-4">
+  dontShowEmpty(prop){ //Can maybe use to generalize if needed? 
+    if(prop.length !== 0){
+      return (<div className="col-md-4">
                   <h4>Items Needed:</h4> <h4 className="renderedResult">{prop}</h4>
               </div>)
-		}
-	}
+    }
+  }
 
-	clearState(){
-		this.setState({renderedGames: null,
-					   difficulty: undefined,
-		    		   time: undefined,
-		    		   type: new ToggleList(),
-		    		   numPlayers: undefined
-		    		}) 
-	}
-	
-	render(){
+  clearState(){
+    this.setState({renderedGames: null,
+             difficulty: undefined,
+               time: undefined,
+               type: new ToggleList(),
+               numPlayers: undefined
+            }) 
+  }
+  
+  render(){
     var carouselId = 1;
     var carouselIndex = 0;
     var self = this;
     var page = _.get(this, 'props.params.page') || 1
     var numPerPage = 10
-		return (
-    			<div>
+    return (
+          <div>
             <nav aria-label="Page navigation">
               <ul className="pagination">
                 <li>
@@ -63,11 +63,11 @@ class RenderedGames extends React.Component {
                 </li>
               </ul>
             </nav>
-      			<div>
-      				<button className="btn btn-success btn-lg" onClick={() => browserHistory.push('/')}>Search Again</button>
-    			</div>
-      			<ul className="list-group">
-      				{(this.props.renderedGames.slice((page-1) * numPerPage, (page) * numPerPage)|| []).map(function(game) { 
+            <div>
+              <button className="btn btn-success btn-lg" onClick={() => browserHistory.push('/')}>Search Again</button>
+          </div>
+            <ul className="list-group">
+              {(this.props.renderedGames.slice((page-1) * numPerPage, (page) * numPerPage)|| []).map(function(game) { 
                   carouselId++;
                   carouselIndex = 0
                   return (
@@ -81,7 +81,7 @@ class RenderedGames extends React.Component {
                           </div>
                           
                           <div className="col-md-4">
-  								   		    <h4>Percent Match:</h4> <h4 className="renderedResult">{game.totalScore}%</h4>  
+                            <h4>Percent Match:</h4> <h4 className="renderedResult">{game.totalScore}%</h4>  
                           </div>
                         </div>
                         <div className="row">
@@ -94,7 +94,7 @@ class RenderedGames extends React.Component {
                         <div className="row">
                           <div className="col-md-12">
                            <h4>Description:</h4> <h4 className="renderedResult">{game.description}</h4>
-  							   	      </div>
+                          </div>
                         </div>
 
                         <div className="row">
@@ -144,6 +144,7 @@ class RenderedGames extends React.Component {
                         </div>
                       </div>
                     </div> 
+                    
                   </div>
 
                   ) 
@@ -151,10 +152,27 @@ class RenderedGames extends React.Component {
               
             )
           }
+          <nav aria-label="Page navigation">
+            <ul className="pagination">
+              <li>
+                <Link to={`/results/${page-1 ? page -1 : page }`}>
+                  <span aria-hidden="true">&laquo;</span>
+                </Link>
+              </li>
+              {
+                _.range(1, Math.ceil(self.props.renderedGames.length / numPerPage) + 1).map((x) => <li><Link to={`/results/${x}`}>{x}</Link></li>)
+              }
+              <li>
+                <Link to={`/results/${parseInt(page) + 1 > Math.ceil(self.props.renderedGames.length / numPerPage) ? page : parseInt(page) + 1}`}>
+                  <span aria-hidden="true">&raquo;</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </ul>
-    	</div>
+      </div>
     )
-	}
+  }
 }
 
 module.exports = RenderedGames
